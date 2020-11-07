e6360ae9eef491162b2868dc8d6d60102a6fbe87
eduar <eduardo.oneill@upr.edu>
2020-11-07 13:39:24 -0400

    [FEAT] Create routes for posting petitions and updating petitions

--- src/routes/petitions/petitions.controller.ts
+++ src/routes/petitions/petitions.controller.ts
@@ -3,16 +3,37 @@ import { BookPetition } from "../../models/petition";
 
 export class PetitionsController {
 
-    public async searchPetitions(status?: string, lending?: number, selling?: number, currentLocation?: number, locationRadius?: number, page: number = 1, limit: number = 25): Promise<BookPetition[]> {
+    public async searchPetitions(status?: string, lending?: number, selling?: number, currentLocation?: number, page: number = 1, limit: number = 25): Promise<BookPetition[]> {
         return new Promise<BookPetition[]>((resolve, reject) => {
+            console.log(status, lending, selling, currentLocation, page, limit)
+            resolve([])
+        })
+    }
 
+    public async getPetitionById(petitionId: string): Promise<BookPetition> {
+        return new Promise<BookPetition>((resolve, reject) => {
+            console.log(petitionId)
+            resolve()
         })
     }
 
     public async getPetitionsByUser(userId: string, status?: string, lending?: number, selling?: number, page: number = 1, limit: number = 25): Promise<BookPetition[]> {
         return new Promise<BookPetition[]>((resolve, reject) => {
-
+            console.log(userId, status, lending, selling, page, limit)
         })
     }
 
+    public async insertBookPetition(userId: string, bookId: string, status: string, description: string, lending: number, selling: number, geolocation: [number, number], locationRadius: number, expirationDate: Date): Promise<string> {
+        return new Promise<string>((resolve, reject) => {
+            console.log(status, lending, selling, description, geolocation, locationRadius, expirationDate)
+            resolve('')
+        })
+    }
+
+    public async updateBookPetition(petitionId: string, status?: string, description?: string, lending?: number, selling?: number, geolocation?: [number, number], locationRadius?: number, expirationDate?: Date): Promise<string> {
+        return new Promise<string>((resolve, reject) => {
+            console.log(status, lending, selling, description, geolocation, locationRadius, expirationDate)
+            resolve('')
+        })
+    }
 }
--- src/routes/petitions/petitions.ts
+++ src/routes/petitions/petitions.ts
@@ -18,7 +18,40 @@ petitionsRouter.get('/', (req: Request, res: Response) => {
     });
 })
 
-petitionsRouter.get('/search', async (req: Request, res: Response) => {
+petitionsRouter.post('/', async (req: Request, res: Response): Promise<void> => {
+    try{
+        const userId: string = req.body.userId as string
+        const bookId: string = req.body.bookId as string
+        const description: string = req.body.description as string
+        const lending: number = +(req.body.lending as string)
+        const selling: number = +(req.body.selling as string)
+        const status: string = req.body.status as string
+        const lat: number = +(req.body.lat as string)
+        const lon: number = +(req.body.lon as string)
+        const locationRadius: number = +(req.body.locationRadius as string)
+        const expirationDate: string = req.body.expirationDate as string
+
+        if(!bookId || !description || !lending || !selling || !status || !lat || !lon || !locationRadius || !expirationDate){
+            res.status(400).json({
+                error: 'Missing parameters in body. bookId, status, description, lending, selling, lon, lat, locationRadius, expirationDate are required.'
+            })
+        }
+
+        const bookPetitionId: string = await petitionsController.insertBookPetition(userId, bookId, status, description, lending, selling, [lon, lat], locationRadius, new Date(expirationDate))
+        
+        res.status(201).json({
+            success: true,
+            bookPetitionId: bookPetitionId
+        })
+
+    } catch(error){
+        res.status(500).json({
+            error: error.message
+        })
+    }
+})
+
+petitionsRouter.get('/search', async (req: Request, res: Response): Promise<void> => {
     try {
         const page: number | undefined = +(req.query.page as string) || undefined
         const limit: number | undefined = +(req.query.limit as string) || undefined
@@ -27,10 +60,9 @@ petitionsRouter.get('/search', async (req: Request, res: Response) => {
         const lending: number | undefined = +(req.query.lending as string) || undefined
         const status: string = req.query.status as string
         const currentLocation: number | undefined = +(req.query.currentLocation as string) || undefined
-        const locationRadius: number | undefined = +(req.query.locationRadius as string) || undefined
 
-        const petitions: BookPetition[] = await petitionsController.searchPetitions(status, lending, selling, currentLocation, locationRadius, page, limit)
-
+        const petitions: BookPetition[] = await petitionsController.searchPetitions(status, lending, selling, currentLocation, page, limit)
+        
         res.json({
             data: petitions
         });
@@ -42,7 +74,7 @@ petitionsRouter.get('/search', async (req: Request, res: Response) => {
     }
 })
 
-petitionsRouter.get('/:userId', async (req: Request, res: Response) => {
+petitionsRouter.get('/user/:userId', async (req: Request, res: Response): Promise<void> => {
     try{
         const userId: string = req.params.userId
         const page: number | undefined = +(req.query.page as string) || undefined
@@ -58,6 +90,49 @@ petitionsRouter.get('/:userId', async (req: Request, res: Response) => {
             data: bookPetitions
         })
     } catch(error) {
+        res.status(500).json({
+            error: error.message
+        })
+    }
+})
+
+petitionsRouter.get('/:petitionId', async (req: Request, res: Response): Promise<void> => {
+    try {
+        const petitionId: string = req.params.petitionId
+
+        const petition: BookPetition = await petitionsController.getPetitionById(petitionId)
+
+        res.json({
+            data: petition
+        });
+
+    } catch(error) {
+        res.status(500).json({
+            error: error.message
+        })
+    }
+})
+
+petitionsRouter.put('/:petitionId', async (req: Request, res: Response) => {
+    try{
+        const petitionId: string = req.params.petitionId
+        const description: string = req.body.description as string
+        const lending: number = +(req.body.lending as string)
+        const selling: number = +(req.body.selling as string)
+        const status: string = req.body.status as string
+        const lat: number = +(req.body.lat as string)
+        const lon: number = +(req.body.lon as string)
+        const locationRadius: number = +(req.body.locationRadius as string)
+        const expirationDate: Date | undefined = req.body.expirationDate as string ? new Date(req.body.expirationDate as string) : undefined
+
+        const bookPetitionId: string = await petitionsController.updateBookPetition(petitionId, status, description, lending, selling, [lon, lat], locationRadius, expirationDate)
+        
+        res.status(204).json({
+            success: true,
+            bookPetitionId: bookPetitionId
+        })
+
+    } catch(error){
         res.status(500).json({
             error: error.message
         })
