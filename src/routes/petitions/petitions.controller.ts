895c9027d76113311320ff4db6a9fb86df2e5ebd
eduar <eduardo.oneill@upr.edu>
2020-11-07 10:53:03 -0400

    [FEAT] Create book petition search route

---
+++ src/routes/petitions/petitions.controller.ts
@@ -0,0 +1,12 @@
+import { BookPetition } from "../../models/petition";
+
+
+export class PetitionsController {
+
+    public async searchPetitions(status?: string, lending?: number, selling?: number, currentLocation?: number, locationRadius?: number, page: number = 1, limit: number = 25): Promise<BookPetition[]> {
+        return new Promise<BookPetition[]>((resolve, reject) => {
+            
+        })
+    }
+
+}
---
+++ src/routes/petitions/petitions.ts
@@ -0,0 +1,43 @@
+import { BookPetition } from "@src/models/petition";
+import express, { Request, Response } from "express";
+import { PetitionsController } from './petitions.controller'
+/**
+ * Router Definition
+ */
+export const petitionsRouter = express.Router();
+
+/**
+ * Routes and method definitions
+ */
+const petitionsController: PetitionsController = new PetitionsController()
+
+petitionsRouter.get('/', (req: Request, res: Response) => {
+    res.json({
+        message: 'This is the petition endpoint',
+        query: req.query
+    });
+})
+
+petitionsRouter.get('/search', async (req: Request, res: Response) => {
+    try {
+        const page: number | undefined = +(req.query.page as string) || undefined
+        const limit: number | undefined = +(req.query.limit as string) || undefined
+    
+        const selling: number | undefined = +(req.query.selling as string) || undefined
+        const lending: number | undefined = +(req.query.lending as string) || undefined
+        const status: string = req.query.status as string
+        const currentLocation: number | undefined = +(req.query.currentLocation as string) || undefined
+        const locationRadius: number | undefined = +(req.query.locationRadius as string) || undefined
+
+        const petitions: BookPetition[] = await petitionsController.searchPetitions(status, lending, selling, currentLocation, locationRadius, page, limit)
+
+        res.json({
+            data: petitions
+        });
+
+    } catch(error) {
+        res.status(500).json({
+            error: error.message
+        })
+    }
+})
