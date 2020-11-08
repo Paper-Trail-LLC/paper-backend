3cea168444ec18b95950ad24015a750915d0f719
eduar <eduardo.oneill@upr.edu>
2020-11-08 15:20:49 -0400

    [FEAT] Implement petition controller functions

--- src/routes/petitions/petitions.controller.ts
+++ src/routes/petitions/petitions.controller.ts
@@ -1,39 +1,197 @@
 import { BookPetition } from "../../models/petition";
-
+import myPool from "../../helpers/mysql.pool"
 
 export class PetitionsController {
 
-    public async searchPetitions(status?: string, lending?: number, selling?: number, currentLocation?: number, expired?: boolean, page: number = 1, limit: number = 25): Promise<BookPetition[]> {
+    public async searchPetitions(expired: boolean, status?: string, lending?: number, selling?: number, currentLocation?: [number, number], searchRadius?: number, page: number = 1, limit: number = 25): Promise<BookPetition[]> {
         return new Promise<BookPetition[]>((resolve, reject) => {
-            console.log(status, lending, selling, currentLocation, expired, page, limit)
-            resolve([])
+            const query = `select * from book_petition 
+            where expiration_date ${expired? '<':'>'} now() 
+            ${status? 'and status = ? ':''}
+            ${lending != undefined? 'and lending = ? ':''}
+            ${selling != undefined? 'and selling = ? ':''}
+            ${currentLocation? 'and ST_Distance(Point(?, ?), geolocation) <= ? and (location_radius is null or (ST_Distance(Point(?, ?), geolocation) <= location_radius) ':'and location_radius is null '}
+            limit ? 
+            offset ?;`
+
+            const v = []
+            if(status) v.push(status)
+            if(lending != undefined) v.push(lending)
+            if(selling != undefined) v.push(selling)
+            if(currentLocation) {
+                v.push(currentLocation[0])
+                v.push(currentLocation[1])
+                v.push(searchRadius)
+                v.push(currentLocation[0])
+                v.push(currentLocation[1])
+            }
+            v.push(limit)
+            v.push(limit*(page-1))
+
+            myPool.query({
+                sql: query,
+                values: v
+            }, (error, results: Array<any>) => {
+                if(error){
+                    reject(error)
+                } else {
+                    const bookPetitions: BookPetition[] = results.map<BookPetition>((value) => {
+                        return new BookPetition(
+                            value['book_id'],
+                            value['user_id'],
+                            value['description'],
+                            value['lending'],
+                            value['selling'],
+                            value['status'], 
+                            [value['geolocation'].x, value['geolocation'].y],
+                            value['location_radius'],
+                            value['expiration_date'],
+                            value['created_on']
+                        )
+                    })
+                    resolve(bookPetitions)
+                }
+            })
         })
     }
 
     public async getPetitionById(petitionId: string): Promise<BookPetition> {
         return new Promise<BookPetition>((resolve, reject) => {
-            console.log(petitionId)
-            resolve()
+            const query = `select * from book_petition 
+            where id = ?;`
+
+            myPool.query({
+                sql: query,
+                values: [petitionId]
+            }, (error, results: Array<any>) => {
+                if(error){
+                    reject(error)
+                } else {
+                    const bookPetitions: BookPetition[] = results.map<BookPetition>((value) => {
+                        return new BookPetition(
+                            value['book_id'],
+                            value['user_id'],
+                            value['description'],
+                            value['lending'],
+                            value['selling'],
+                            value['status'], 
+                            [value['geolocation'].x, value['geolocation'].y],
+                            value['location_radius'],
+                            value['expiration_date'],
+                            value['created_on']
+                        )
+                    })
+                    resolve(bookPetitions[0])
+                }
+            })
         })
     }
 
-    public async getPetitionsByUser(userId: string, status?: string, lending?: number, selling?: number, expired?: boolean, page: number = 1, limit: number = 25): Promise<BookPetition[]> {
+    public async getPetitionsByUser(userId: string, expired: boolean, status?: string, lending?: number, selling?: number, page: number = 1, limit: number = 25): Promise<BookPetition[]> {
         return new Promise<BookPetition[]>((resolve, reject) => {
-            console.log(userId, status, lending, selling, expired, page, limit)
+            const query = `select * from book_petition 
+            where user_id = ? 
+            and expiration_date ${expired? '<':'>'} now() 
+            ${status? 'and status = ? ':''}
+            ${lending != undefined? 'and lending = ? ':''}
+            ${selling != undefined? 'and selling = ? ':''}
+            limit ? 
+            offset ?;`
+
+            const v = []
+            v.push(userId)
+            if(status) v.push(status)
+            if(lending != undefined) v.push(lending)
+            if(selling != undefined) v.push(selling)
+            v.push(limit)
+            v.push(limit*(page-1))
+
+            myPool.query({
+                sql: query,
+                values: v
+            }, (error, results: Array<any>) => {
+                if(error){
+                    reject(error)
+                } else {
+                    const bookPetitions: BookPetition[] = results.map<BookPetition>((value) => {
+                        return new BookPetition(
+                            value['book_id'],
+                            value['user_id'],
+                            value['description'],
+                            value['lending'],
+                            value['selling'],
+                            value['status'], 
+                            [value['geolocation'].x, value['geolocation'].y],
+                            value['location_radius'],
+                            value['expiration_date'],
+                            value['created_on']
+                        )
+                    })
+                    resolve(bookPetitions)
+                }
+            })
         })
     }
 
     public async insertBookPetition(userId: string, bookId: string, status: string, description: string, lending: number, selling: number, geolocation: [number, number], locationRadius: number, expirationDate: Date): Promise<string> {
         return new Promise<string>((resolve, reject) => {
-            console.log(status, lending, selling, description, geolocation, locationRadius, expirationDate)
-            resolve('')
+            let query = `set @petitionId = uuid_to_bin(uuid()); 
+            insert into book_petition (user_id, book_id, status, description, lending, selling, geolocation, location_radius, expiration_date) 
+            values (?, ?, ?, ?, ?, ?, ST_GeomFromText('Point(? ?)'), ?, ?); 
+            select @petitionId;`
+
+            myPool.query({
+                sql: query,
+                values: [userId, bookId, status, description, lending, selling, geolocation[0], geolocation[1], locationRadius, expirationDate]
+            }, (error, results: Array<Array<any>>) => {
+                if(error){
+                    reject(error)
+                } else {
+                    resolve(results[2][0]['@petitionId'])
+                }
+            })
         })
     }
 
-    public async updateBookPetition(petitionId: string, status?: string, description?: string, lending?: number, selling?: number, geolocation?: [number, number], locationRadius?: number, expirationDate?: Date): Promise<string> {
-        return new Promise<string>((resolve, reject) => {
-            console.log(status, lending, selling, description, geolocation, locationRadius, expirationDate)
-            resolve('')
+    public async updateBookPetition(petitionId: string, status?: string, description?: string, lending?: number, selling?: number, geolocation?: [number, number], locationRadius?: number, expirationDate?: Date): Promise<boolean> {
+        return new Promise<boolean>((resolve, reject) => {
+            let query = `update book_petition set 
+            ${status? 'status = ? ':''}
+            ${description? 'description = ? ':''}
+            ${lending != undefined? 'lending = ? ':''}
+            ${selling != undefined? 'selling = ? ':''}
+            ${geolocation? `geolocation = ST_GeomFromText('Point(? ?)') `:''}
+            ${locationRadius? 'location_radius = ? ':''}
+            ${expirationDate? 'expiration_date = ? ':''}
+            where id = ?;`
+            
+            const v = []
+            if(status) v.push(status)
+            if(description) v.push(description)
+            if(lending != undefined) v.push(lending)
+            if(selling != undefined) v.push(selling)
+            if(geolocation) {
+                v.push(geolocation[0])
+                v.push(geolocation[1])
+            }
+            if(locationRadius) v.push(locationRadius)
+            if(expirationDate) v.push(expirationDate)
+            v.push(petitionId)
+
+            myPool.query({
+                sql: query,
+                values: v
+            }, (error, results) => {
+                if(error){
+                    reject(error)
+                } else {
+                    if(results.affectedRows == 0){
+                        resolve(false)
+                    } else {
+                        resolve(true)
+                    }
+                }
+            })
         })
     }
 }
--- src/routes/petitions/petitions.ts
+++ src/routes/petitions/petitions.ts
@@ -33,16 +33,17 @@ petitionsRouter.post('/', async (req: Request, res: Response): Promise<void> => 
 
         if(!bookId || !description || !lending || !selling || !status || !lat || !lon || !locationRadius || !expirationDate){
             res.status(400).json({
-                error: 'Missing parameters in body. bookId, status, description, lending, selling, lon, lat, locationRadius, expirationDate are required.'
+                error: 'Missing parameters in body. userId, bookId, status, description, lending, selling, lon, lat, locationRadius, expirationDate are required.'
             })
+            return
         }
 
         const bookPetitionId: string = await petitionsController.insertBookPetition(userId, bookId, status, description, lending, selling, [lon, lat], locationRadius, new Date(expirationDate))
         
-        res.status(201).json({
-            success: true,
-            bookPetitionId: bookPetitionId
-        })
+            res.status(201).json({
+                success: true,
+                bookPetitionId: bookPetitionId
+            })
 
     } catch(error){
         res.status(500).json({
@@ -59,10 +60,24 @@ petitionsRouter.get('/search', async (req: Request, res: Response): Promise<void
         const selling: number | undefined = +(req.query.selling as string) || undefined
         const lending: number | undefined = +(req.query.lending as string) || undefined
         const status: string = req.query.status as string
-        const currentLocation: number | undefined = +(req.query.currentLocation as string) || undefined
+        const currentLat: number | undefined = +(req.query.currentLat as string) || undefined
+        const currentLon: number | undefined = +(req.query.currentLon as string) || undefined
+        const searchRadius: number | undefined = +(req.query.searchRadius as string) || undefined
         const expired: boolean = (req.query.expired as string) === 'true'? true : false
 
-        const petitions: BookPetition[] = await petitionsController.searchPetitions(status, lending, selling, currentLocation, expired, page, limit)
+        let currentLocation: [number, number] | undefined = undefined 
+        if(currentLat && currentLon){
+            currentLocation = [currentLon, currentLat]
+        }
+
+        if(currentLocation && !searchRadius){
+            res.status(400).json({
+                error: 'Must provide currentLat, currentLon and searchRadius'
+            })
+            return
+        }
+
+        const petitions: BookPetition[] = await petitionsController.searchPetitions(expired, status, lending, selling, currentLocation, searchRadius, page, limit)
         
         res.json({
             data: petitions
@@ -86,7 +101,7 @@ petitionsRouter.get('/user/:userId', async (req: Request, res: Response): Promis
         const status: string = req.query.status as string
         const expired: boolean = (req.query.expired as string) === 'true'? true : false
 
-        const bookPetitions: BookPetition[] = await petitionsController.getPetitionsByUser(userId, status, lending, selling, expired, page, limit)
+        const bookPetitions: BookPetition[] = await petitionsController.getPetitionsByUser(userId, expired, status, lending, selling, page, limit)
 
         res.json({
             data: bookPetitions
@@ -119,20 +134,31 @@ petitionsRouter.put('/:petitionId', async (req: Request, res: Response) => {
     try{
         const petitionId: string = req.params.petitionId
         const description: string = req.body.description as string
-        const lending: number = +(req.body.lending as string)
-        const selling: number = +(req.body.selling as string)
+        const lending: number | undefined = +(req.body.lending as string) || undefined
+        const selling: number | undefined = +(req.body.selling as string) || undefined
         const status: string = req.body.status as string
-        const lat: number = +(req.body.lat as string)
-        const lon: number = +(req.body.lon as string)
-        const locationRadius: number = +(req.body.locationRadius as string)
+        const lat: number | undefined = +(req.body.lat as string) || undefined
+        const lon: number | undefined = +(req.body.lon as string) || undefined
+        const locationRadius: number | undefined = +(req.body.locationRadius as string) || undefined
         const expirationDate: Date | undefined = req.body.expirationDate as string ? new Date(req.body.expirationDate as string) : undefined
 
-        const bookPetitionId: string = await petitionsController.updateBookPetition(petitionId, status, description, lending, selling, [lon, lat], locationRadius, expirationDate)
+        let geolocation: [number, number] | undefined = undefined 
+        if(lat && lon){
+            geolocation = [lon, lat]
+        }
+
+        const success: boolean = await petitionsController.updateBookPetition(petitionId, status, description, lending, selling, geolocation, locationRadius, expirationDate)
         
-        res.status(204).json({
-            success: true,
-            bookPetitionId: bookPetitionId
-        })
+        if(success){
+            res.status(204).json({
+                success: true
+            })
+        } else {
+            res.status(400).json({
+                success: false,
+                error: 'Petition id does not match any petitions in the database.'
+            })
+        }
 
     } catch(error){
         res.status(500).json({
