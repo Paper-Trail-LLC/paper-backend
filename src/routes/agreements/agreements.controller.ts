ffd8a05313205ad8683702d76d5a4383afd3c68c
eduar <eduardo.oneill@upr.edu>
2020-11-23 14:01:28 -0400

    [CHORE] Comment unfinished functions

--- src/routes/agreements/agreements.controller.ts
+++ src/routes/agreements/agreements.controller.ts
@@ -1,43 +1,43 @@
 export class AgreementsController {
 
-    public async getAgreementsById(agreementId: string){
+    // public async getAgreementsById(agreementId: string){
 
-    }
+    // }
 
-    //PRIORITY
-    public async searchMeetingAgreements(userId?: string, userBookId?: string, status?: string, distance?: number, hasPassed?: boolean) {
+    // //PRIORITY
+    // public async searchMeetingAgreements(userId?: string, userBookId?: string, status?: string, distance?: number, hasPassed?: boolean) {
 
-    }
+    // }
 
-    public async searchPurchaseAgreements(userId?: string, userBookId?: string, status?: string){
+    // public async searchPurchaseAgreements(userId?: string, userBookId?: string, status?: string){
 
-    }
+    // }
 
-    public async searchBorrowAgreements(userId?: string, userBookId?: string, status?: string, hasPassed?: boolean){
+    // public async searchBorrowAgreements(userId?: string, userBookId?: string, status?: string, hasPassed?: boolean){
 
-    }
+    // }
 
-    public async addRequestToMeetingAgreement(agreementId: string) {
+    // public async addRequestToMeetingAgreement(agreementId: string) {
 
-    }
+    // }
 
-    public async addRequestToPurchaseAgreement(agreementId: string) {
+    // public async addRequestToPurchaseAgreement(agreementId: string) {
         
-    }
+    // }
 
-    public async addRequestToBorrowAgreement(agreementId: string) {
+    // public async addRequestToBorrowAgreement(agreementId: string) {
 
-    }
+    // }
 
-    public async acceptMeetingAgreement(agreementId: string){
+    // public async acceptMeetingAgreement(agreementId: string){
 
-    }
+    // }
 
-    public async acceptPurchaseAgreement(agreementId: string){
+    // public async acceptPurchaseAgreement(agreementId: string){
 
-    }
+    // }
 
-    public async acceptBorrowAgreement(agreementId: string){
+    // public async acceptBorrowAgreement(agreementId: string){
 
-    }
+    // }
 }
